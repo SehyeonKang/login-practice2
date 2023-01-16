e4f3b8e83d54222eea8e79f016664dc2bee74c5c
SehyeonKang <gedfgrdt@naver.com>
2023-01-16 22:59:56 +0900

    20230116 commit

--- src/main/java/com/test/loginpractice/member/MemberRepository.java
+++ src/main/java/com/test/loginpractice/member/MemberRepository.java
@@ -1,4 +1,11 @@
 package com.test.loginpractice.member;
 
-public interface MemberRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Transactional
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByAccount(String account);
 }
