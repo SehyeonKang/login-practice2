a860d3e98b22801b47dde1a34685e48bc9fde3c7
SehyeonKang <gedfgrdt@naver.com>
2023-01-18 23:56:31 +0900

    20230118 commit

--- src/main/java/com/test/loginpractice/member/dto/SignRequest.java
+++ src/main/java/com/test/loginpractice/member/dto/SignRequest.java
@@ -1,4 +1,20 @@
 package com.test.loginpractice.member.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class SignRequest {
+
+    private Long id;
+
+    private String account;
+
+    private String password;
+
+    private String nickname;
+
+    private String name;
+
+    private String email;
 }
